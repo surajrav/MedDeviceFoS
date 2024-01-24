41922a2da7c13ca12d8cf3efe2b7760661c58fde
Suraj Ravichandran <surajravi@gmail.com>
2024-01-23 16:53:12 -0800

    fix: Switch to making the patient images bucket within the app-init script.
    This prevents a race condition between when the minio service is up but hasn't created
    the patient images bucket and the app-init service is tryint to set the same (not-yet-created)
    bucket's policy to public

--- backend/app/app_init.sh
+++ backend/app/app_init.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 SCRIPT_DIR=$(realpath $(dirname $0))
+MC_PATH=${SCRIPT_DIR}/../minio-binaries/mc
 
-# Make the default bucket public so that the client can reach the images
-${SCRIPT_DIR}/../minio-binaries/mc config host add localminio "http://${MINIO_SERVER_HOST}:9000" "${MINIO_SERVER_ACCESS_KEY}" "${MINIO_SERVER_SECRET_KEY}"
-${SCRIPT_DIR}/../minio-binaries/mc anonymous set public "localminio/${PATIENT_IMG_BUCKET}"
+# Create the patient image bucket and make it public so that the client can reach the images
+${MC_PATH} config host add localminio "http://${MINIO_SERVER_HOST}:9000" "${MINIO_SERVER_ACCESS_KEY}" "${MINIO_SERVER_SECRET_KEY}"
+${MC_PATH} mb "localminio/${PATIENT_IMG_BUCKET}"
+${MC_PATH} anonymous set public "localminio/${PATIENT_IMG_BUCKET}"
 
 # If running behind a proxy like Nginx or Traefik add --proxy-headers
-uvicorn app.main:app --proxy-headers --reload --host 0.0.0.0 --port 80 --root-path ${PROXY_PREFIX_PATH}
+uvicorn app.main:app --proxy-headers --reload --host 0.0.0.0 --port 80 --root-path ${PROXY_PREFIX_PATH} || exit 1
